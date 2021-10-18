47c16adb0969be4ce9b9bc78c3c070cba31631e3
Fabian Schnötzinger <fabian.schnoetzinger@outlook.com>
2021-10-18 15:36:10 +0200

    initial commit

---
+++ src/main/java/com/example/backend_verwaltungssoftware/BackEndVerwaltungssoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_verwaltungssoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndVerwaltungssoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndVerwaltungssoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_verwaltungssoftware/BackEndVerwaltungssoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_verwaltungssoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndVerwaltungssoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
